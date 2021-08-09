b87ba2d85b431cb18f26f265c6be392060e77078
Prince <avecillasprince@yahoo.com>
2021-08-08 21:27:54 -0400

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,22 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(x)
+    puts "Hello, #{x}!" 
+end
+
+def greet_with_default(x="programmer")
+    puts "Hello, #{x}!"
+end
+
+def add(x,y)
+    return x+y
+end
+
+def halve(x)
+    return nil unless x.class == Integer
+    return x/2
+    
+end
